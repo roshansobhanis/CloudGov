014c15f4d1dacc194988cd251a009db3b1b3d6cc
roshansobhanis <44311297+roshansobhanis@users.noreply.github.com>
2022-05-02 16:35:11 +0530

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,40 @@
-output "registry_id" {
-    value       = azurerm_container_registry.acr.id
-    description = "The ID of the Container Registry"
+output "aks_id" {
+    value = azurerm_kubernetes_cluster.kube_cluster.id
+    description = "AKS cluster id"
 }
 
-output "registry_name" {
-    value       = azurerm_container_registry.acr.name
-    description = "The name of the Container Registry"
+output "aks_fqdn" {
+    value = azurerm_kubernetes_cluster.kube_cluster.fqdn
+    description = "AKS cluster FQDN"
 }
 
-output "registry_logon_url" {
-    value       = azurerm_container_registry.acr.login_server
-    description = "The URL that can be used to log into the container registry."
+output "node_resource_group" {
+    value = azurerm_kubernetes_cluster.kube_cluster.node_resource_group
+    description = "Newly created node resource group name where all components will be located"
+}
+
+output "master_principal_id" {
+    value = azurerm_kubernetes_cluster.kube_cluster.identity.0.principal_id
+    description = "AKS Managed Identity service principal ID"
+}
+
+output "master_tenant_id" {
+    value = azurerm_kubernetes_cluster.kube_cluster.identity.0.tenant_id
+    description = "AKS Managed Identity service principal ID"
+}
+
+output "kubelet_identity_client_id" {
+    value = azurerm_kubernetes_cluster.kube_cluster.kubelet_identity.0.client_id
+}
+
+output "kubelet_identity_object_id" {
+    value = azurerm_kubernetes_cluster.kube_cluster.kubelet_identity.0.object_id
+}
+
+output "kube_admin_config_hostname" {
+    value = azurerm_kubernetes_cluster.kube_cluster.kube_admin_config.0.host
+}
+
+output "kube_admin_config_username" {
+    value = azurerm_kubernetes_cluster.kube_cluster.kube_admin_config.0.username
 }
